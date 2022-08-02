8467c907f0b55beb70f7236d233dad34715aab55
Bernardngari <bernardngari.ke@gmail.com>
2022-08-02 18:05:03 +0300

    Set the toggle state for instock/out of stock

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({image,name,price}) {
+  const[instock, setInStock] = useState(true);
+  const toggleInStock = () =>{setInStock(()=>!instock)}
   return (
     <li className="card">
       <img src={image} alt={"plant name"} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {instock? (
+        <button onClick={toggleInStock} className="primary">{instock? "In Stock":"Out of Stock"}</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={toggleInStock}>{instock? "In Stock":"Out of Stock"}</button>
       )}
     </li>
   );
